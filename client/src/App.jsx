85d7929f193d9b8b01ce8148266886f178fc6fb9
Abhay-Kumar30 <your.abhaykumar1gkp@gmail.com>
2026-01-10 22:39:10 +0530

    Page routes setup

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
